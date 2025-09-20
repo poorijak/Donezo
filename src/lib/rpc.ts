005a2a69fb82289f8ac917e6a28780e8400d121e
Sommoi <poorijak35@gmail.com>
2025-09-21 01:28:36 +0700

    switch rpc

--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -1,5 +1,5 @@
 import { appType } from "@/app/api/[[...route]]/route";
 import { hc } from "hono/client";
 
-// export const client = hc<appType>(process.env.NEXT_PUBLIC_VERCEL_URL!);
-export const client = hc<appType>(process.env.NEXT_PUBLIC_BASE_URL!);
+export const client = hc<appType>(process.env.NEXT_PUBLIC_VERCEL_URL!);
+// export const client = hc<appType>(process.env.NEXT_PUBLIC_BASE_URL!);
