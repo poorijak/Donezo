9d3b85fd32315e1a8ca3f20edadcc062ccf7475c
Sommoi <poorijak35@gmail.com>
2025-09-25 10:13:20 +0700

    fix drag item in desktop

--- src/feature/task/components/dnd/task-card.tsx
+++ src/feature/task/components/dnd/task-card.tsx
@@ -45,7 +45,7 @@ const TaskCard = ({ task }: TaskCardProps) => {
   return (
     <div className="w-full">
       <div
-          {...(!isMobile ? { ...attributes, ...listeners } : {})}
+          {...(isMobile ? { ...attributes, ...listeners } : {})}
         ref={isMobile ? setNodeRef : null}
         style={style}
         className="bg-card relative cursor-grab rounded-md border p-4 shadow-sm"
